d2703e47799bc1ed893cb3c2afd76fde3f50d452
maxi436 <maxi@sanjuan-schroeder.de>
2026-06-04 11:36:23 +0000

    Update Website + Add Debug Mode

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function show(section){
+function show(section, target){
     document.querySelectorAll('section').forEach(s => s.style.display = 'none');
     document.querySelectorAll('.navbutton').forEach(b => b.classList.remove('selected'));
     document.querySelector(`.${section}`).style.display = 'block';
-    event.target.classList.add('selected');
+    if (target) target.classList.add('selected');
 }
-show('start');
+show('start', document.querySelector('.navbutton.selected'));
